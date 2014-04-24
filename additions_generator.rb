aecf03cbcfd3e698daf5fd8c94f169e48935d4a8
Ben Walford <bpwalford@gmail.com>
2014-04-24 10:51:08 -0600

    fixed file location from last commit

--- additions_generator.rb
+++
@@ -1,8 +0,0 @@
-class AdditionsGenerator < Rails::Generator::Base
-	def manifest
-		record do |m|
-			m.file "jquery-letterfx.js", "pulbic/javascripts/"
-			m.file "jquery-letterfx.js", "pulbic/stylesheets/"
-		end
-	end
-end
